0141d1011d0ed77f70d854b339f708cb972af93b
Dai Dang Van <daidv@vn.fujitsu.com>
2017-02-24 11:20:23 +0700

    Format result

--- test_swift_http_whileloop.py
+++ test_swift_http_whileloop.py
@@ -23,17 +23,46 @@ success = 0
 session = FuturesSession()
 tasks = []
 
+
 def bg_cb(sess, resp):
-    print("%d - %s" % (time.time() * 1000, resp.status_code))
+    "Callback function when requests done"
+
+    timestamp = time.time() * 1000
+    tasks.append({
+        "timestamp": timestamp,
+        "status": resp.status_code
+    })
+    print("%d - %d" % (timestamp, resp.status_code))
+
+def footer():
+    "Return result of testing process"
+
+    is_find_start = True
+    count = 0
+    start, end = 0, 0 # assign this vars prepare if we dont' have downtime
+
+    for task in tasks:
+        if is_find_start:
+            if task.get('status') >= 400:
+                is_find_start = False
+                start = task.get('timestamp')
+        else:
+            count += 1
+            if task.get('status') == 200:
+                end = task.get('timestamp')
+                break
+
+    print("Downtime for rolling upgrade process: {} ms" .format(end-start))
+    print("Number of fail requests (status: 503): {}" .format(count))
 
 
 def exit_gracefully(signum, frame):
+    # Source: Antti Haapala - http://stackoverflow.com/a/18115530
     signal.signal(signal.SIGINT, original_sigint)
 
     try:
         if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
-	    for task in tasks:
-		print(task.done())
+            footer()
             sys.exit(1)
 
     except KeyboardInterrupt:
@@ -50,4 +79,3 @@ if __name__ == '__main__':
     while(True):
     	time.sleep(0.3)
     	future = session.get(url, headers=headers, background_callback=bg_cb)
-	tasks.append(future)
