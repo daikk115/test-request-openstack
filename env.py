8c1ac28f1542aa3bce742273fcd1a0f46c1c8787
Dang Van Dai <daikk115@gmail.com>
2017-02-24 10:28:35 +0700

    Implement graceful stop test script

--- env.py
+++ env.py
@@ -2,9 +2,9 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client
 
-IP = '192.168.122.150'
+IP = '10.164.178.141'
 USERNAME = 'admin'
-PASSWORD = '8XXumMXUe7JglJb4YQ7uHdokhJmvTw3YUJSGbFVY'
+PASSWORD = '1'
 PROJECT_NAME = 'admin'
 AUTH_URL = 'http://{}:5000/v3'.format(IP)
 
--- test_swift_http_whileloop.py
+++ test_swift_http_whileloop.py
@@ -1,13 +1,15 @@
 import requests
 import time
+import signal
+import sys
 
 from requests_futures.sessions import FuturesSession
 
 import env
 
-ACCOUNT = 'AUTH_0064b9fb05d047e88d7a1fc900a9f8a7' #Run 'swift stat' command to see ACCOUNT
+ACCOUNT = 'AUTH_17f007bcbbeb4c219cbeb4776302621c' #Run 'swift stat' command to see ACCOUNT
 CONTAINER = 'DAIKK'
-OBJECT = 'abc.txt'
+OBJECT = 'admin.sh'
 
 
 url = 'http://{}:8080/v1/{}/{}/{}'.format(env.IP, ACCOUNT, CONTAINER, OBJECT)
@@ -18,16 +20,34 @@ headers = {
 
 fail = 0
 success = 0
-
+session = FuturesSession()
+tasks = []
 
 def bg_cb(sess, resp):
     print("%d - %s" % (time.time() * 1000, resp.status_code))
 
-session = FuturesSession()
-tasks = []
 
-while(True):
-	time.sleep(0.3)
-	future = session.get(url, headers=headers, background_callback=bg_cb)
-        tasks.append(future)
+def exit_gracefully(signum, frame):
+    signal.signal(signal.SIGINT, original_sigint)
 
+    try:
+        if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+	    for task in tasks:
+		print(task.done())
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        print("Ok ok, quitting")
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+
+if __name__ == '__main__':
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+    while(True):
+    	time.sleep(0.3)
+    	future = session.get(url, headers=headers, background_callback=bg_cb)
+	tasks.append(future)
