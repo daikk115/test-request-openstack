ce6a9b9c7a8d836a2d9645538652787982b37c42
Dai Dang Van <daidv@vn.fujitsu.com>
2017-03-22 10:52:09 +0700

    temporary test swift CRUD

--- test_swift_http_whileloop.py
+++ test_swift_http_whileloop.py
@@ -1,22 +1,35 @@
-import requests
-
+from os.path import getmtime
 import env
 from graceful_exit import *
 
-
-ACCOUNT = 'AUTH_17f007bcbbeb4c219cbeb4776302621c' #Run 'swift stat' command to see ACCOUNT
-CONTAINER = 'DAIKK'
-OBJECT = 'admin.sh'
-
-
+## CONFIG
+ACCOUNT = 'AUTH_bc009afedb7b4ff0886b9bf18be3e733'  # Run 'swift stat' command to see ACCOUNT
+CONTAINER = 'daikk'
+OBJECT = 'test_object'
+path = 'test_object'
 url = 'http://{}:8080/v1/{}/{}/{}'.format(env.IP, ACCOUNT, CONTAINER, OBJECT)
 
-headers = {
-  'X-Auth-Token': env.TOKEN
+put_headers = {
+    'x-object-meta-mtime': "%f" % getmtime(path),
+    'X-Auth-Token': env.TOKEN
 }
 
+headers = {
+    'X-Auth-Token': env.TOKEN
+}
 
+## RUN
 if __name__ == '__main__':
-    while(True):
-    	time.sleep(0.3)
-    	send_get_request(url, headers=headers)
+    while True:
+        time.sleep(0.1)
+        # Create object
+        f = open(path, 'rb')
+        send_request(url, 'PUT',
+                     headers=put_headers, data=f)
+        f.close()
+        # Update object
+        #send_request(url, 'POST', headers=headers)
+        # Get object
+        send_request(url, 'GET', headers=headers)
+        # Delete object
+        send_request(url, 'DELETE', headers=headers)
